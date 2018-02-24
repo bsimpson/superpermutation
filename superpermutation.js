94d9ea8459e7f0124375444a3e777d09465e3450
Ben Simpson <thehoagie@gmail.com>
2018-02-23 23:19:11 -0500

    I think I got this working with Node cluster

--- superpermutation.js
+++ superpermutation.js
@@ -1,50 +1,58 @@
 module.exports = (() => {
   function calc(val) {
-    let i = 1;
-    const a = Array.from({length: val}, () => i++);
-    let products = [];
-    let generator1 = permutations(a);
-    while((p = generator1.next().value) !== undefined) {
-      products.push(p);
-    }
-    products = products.map((x) => x.join('')); // ["12", "21"]
 
-    let generator2 = permutations(products); // [['12', '21'], ['21', '12']]
-    let shortestString = null;
-    let testString = '';
+    const cluster = require('cluster');
 
-    while ((p = generator2.next().value) != undefined) { // ['12', '21']
-      testString = p.reduce((sum, product) => { // '12'
-        if (sum.search(product) > -1) {
-          // string already contains combination - do nothing
-          return sum;
-        } else {
-          // concat new pattern
-          sum = sum.concat(product);
-
-          // remove adjacent dups
-          let sumArray = sum.split(''); // ['1','2','2','1']
-          let uniqueArray = [];
-          let previousVal = null;
-
-          sumArray.forEach((x) => {
-            if (previousVal !== x) {
-              uniqueArray.push(x);
-            }
-            previousVal = x;
-          });
-          sum = uniqueArray.join(''); // '121'
-        }
-        return sum;
-      }, '');
-
-      if (shortestString == null || (shortestString.length > testString.length)) {
-        console.log(`New shortest superpermutation: '${shortestString}'`);
-        shortestString = testString;
+    if (cluster.isMaster) {
+      let i = 1;
+      const a = Array.from({length: val}, () => i++);
+      let products = [];
+      let generator1 = permutations(a);
+      while((p = generator1.next().value) !== undefined) {
+        products.push(p);
       }
-    };
+      products = products.map((x) => x.join('')); // ["12", "21"]
 
-    return shortestString;
+      let generator2 = permutations(products); // [['12', '21'], ['21', '12']]
+      let shortestString = null;
+
+      // Worker setup
+      const numCPUs = require('os').cpus().length;
+
+      // Fork workers.
+      for (let i = 0; i < numCPUs; i++) {
+        let worker = cluster.fork();
+
+        // Receive message from workers
+        worker.on('message', (testString) => {
+          // console.log(`From worker ${testString}`);
+          if (shortestString == null || (shortestString.length > testString.length)) {
+            shortestString = testString;
+            console.log(`New shortest superpermutation: '${shortestString}'`);
+          }
+        });
+      }
+      // End worker setup
+
+      // Send message to workers
+      while ((p = generator2.next().value) != undefined) { // ['12', '21']
+        let id = Math.floor(Math.random() * numCPUs) + 1;
+        cluster.workers[id].send(p);
+      };
+
+      for (const id in cluster.workers) {
+        cluster.workers[id].kill();
+      }
+      // return `Final result ${shortestString}`;
+
+    } else {
+      process.on('message', (p) => {
+        let data = buildTestString(p);
+        process.send(data);
+      });
+      // cluster.worker.kill();
+      return 'Worker exiting';
+    }
   }
 
   function pretty(val) {
@@ -65,6 +73,32 @@ module.exports = (() => {
         }
       }
     }
+  };
+
+  function buildTestString(p) {
+    return(p.reduce((sum, product) => { // '12'
+      if (sum.search(product) > -1) {
+        // string already contains combination - do nothing
+        return sum;
+      } else {
+        // concat new pattern
+        sum = sum.concat(product);
+
+        // remove adjacent dups
+        let sumArray = sum.split(''); // ['1','2','2','1']
+        let uniqueArray = [];
+        let previousVal = null;
+
+        sumArray.forEach((x) => {
+          if (previousVal !== x) {
+            uniqueArray.push(x);
+          }
+          previousVal = x;
+        });
+        sum = uniqueArray.join(''); // '121'
+      }
+      return sum;
+    }, ''));
   };
 
   return {
